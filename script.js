f0016d1a503919d7ae6c57499d02df216c5a4c20
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-04-15 19:55:04 +0200

    Wersja mobilna

---
+++ script.js
@@ -0,0 +1,7 @@
+var menuButton = document.getElementById("menu");
+var menu = document.querySelector(".navlinks");
+
+
+menuButton.addEventListener("click",function(){
+    menu.classList.toggle("mobile");
+})
