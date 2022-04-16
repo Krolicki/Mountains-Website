22c1cbdaf1568d70e8f5dc0f4e1bb0af621f766f
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-04-16 15:35:23 +0200

    Przycisk powrotu na górę strony

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var menuButton = document.getElementById("menu");
-var menu = document.querySelector(".navlinks");
+const menuButton = document.getElementById("menu");
+const menu = document.querySelector(".navlinks");
+const toTop = document.querySelector(".top");
 
+var active = false;
+
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset > 100){
+        if(active == false){
+            toTop.classList.add("active");
+            active = true;
+            if(menu.classList.contains("mobile"))
+                menu.classList.remove("mobile");
+        }
+    }
+    else{
+        if(active == true){
+            toTop.classList.remove("active");
+            active = false;
+        }
+    }
+})
 
 menuButton.addEventListener("click",function(){
     menu.classList.toggle("mobile");
