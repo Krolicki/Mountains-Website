ac343437ef8fba4d4bd3bf61aacbd6afb48cc91e
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-04-18 11:36:07 +0200

    Pliki Cookies + baner

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const menuButton = document.getElementById("menu");
 const menu = document.querySelector(".navlinks");
 const toTop = document.querySelector(".top");
 const header = document.querySelector(".header-content");
+const cookieBox = document.querySelector(".cookie");
+const acceptCookie = document.querySelector(".acceptCookie");
 
 var active = false;
+
+acceptCookie.addEventListener("click", function(){
+    document.cookie = "TestCookie=Accept; max-age="+60*60+";path=/";
+    if(document.cookie){
+        cookieBox.classList.add("hide");
+    }
+})
+
+let checkCookie = document.cookie.indexOf("TestCookie=Accept");
+if(checkCookie == -1){
+    cookieBox.classList.add("add");
+    cookieBox.classList.remove("hide");
+}
 
 window.onload = function(){ 
     header.classList.add("anim");
